6467d401b9eea186ffeda8dcb3442c55c0021c62
OleksandraMahera <126208886+OleksandraMahera@users.noreply.github.com>
2023-08-05 18:52:29 +0300

    js-hw-09

---
+++ src/js/02-timer.js
@@ -0,0 +1,77 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from "notiflix";
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, "0");
+}
+
+const datePicker = flatpickr("#datetime-picker", {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    const currentDate = new Date();
+
+    if (selectedDate <= currentDate) {
+      Notiflix.Notify.failure("Please choose a date in the future");
+      return;
+    }
+
+    startButton.disabled = false;
+  },
+});
+
+const startButton = document.querySelector('[data-start]');
+const daysValue = document.querySelector('[data-days]');
+const hoursValue = document.querySelector('[data-hours]');
+const minutesValue = document.querySelector('[data-minutes]');
+const secondsValue = document.querySelector('[data-seconds]');
+
+let countdownInterval;
+
+startButton.addEventListener('click', () => {
+  const selectedDate = datePicker.selectedDates[0];
+  const currentDate = new Date();
+  
+  if (selectedDate <= currentDate) {
+    Notiflix.Notify.failure("Please choose a date in the future");
+    return;
+  }
+
+  startButton.disabled = true;
+  countdownInterval = setInterval(() => {
+    const timeLeft = selectedDate - new Date();
+    if (timeLeft <= 0) {
+      clearInterval(countdownInterval);
+      daysValue.textContent = '00';
+      hoursValue.textContent = '00';
+      minutesValue.textContent = '00';
+      secondsValue.textContent = '00';
+      return;
+    }
+    
+    const { days, hours, minutes, seconds } = convertMs(timeLeft);
+    daysValue.textContent = addLeadingZero(days);
+    hoursValue.textContent = addLeadingZero(hours);
+    minutesValue.textContent = addLeadingZero(minutes);
+    secondsValue.textContent = addLeadingZero(seconds);
+  }, 1000);
+});
+
